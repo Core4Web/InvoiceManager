997d19d9d9414f40ec3ddc578de6d5f14320d078
Core4Web <core.4web@gmail.com>
2018-09-19 10:22:54 +0300

    Added simple test invoice controller and view

---
+++ InvoiceManager/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InvoiceManager.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IInvoiceRepository _invoiceRepo;
+        public HomeController(IInvoiceRepository invoiceRepo)
+        {
+            _invoiceRepo = invoiceRepo;
+        }
+        public IActionResult Index() => View(_invoiceRepo.Invoices);
+    }
+}
--- InvoiceManager/Models/Invoice.cs
+++ InvoiceManager/Models/Invoice.cs
@@ -4,6 +4,8 @@ namespace InvoiceManager.Models {
     public class Invoice
     {
         public int Id { get; set; }
+        public string Number { get; set; }
+        public decimal Sum { get; set; }
         public string Note { get; set; }
         public DateTime Created { get; set; }
         public DateTime? Updated { get; set; }
